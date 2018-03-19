0e4dbcc89c48b2455ea1b915aa6706f4229de93b
Isaac Shoman and William Chu <william@>
2018-03-19 11:45:18 -0700

    Add cups to quarts converter and add tablespoons to teaspoons converter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 var gallonInput = parseInt(prompt("Input gallons to be converted to liters:"));
 
+var cupInput = parseInt(prompt("Input cups to be converted to quarts"));
+
+var tablespoonInput = parseInt(prompt("Input tablespoons to be converted to teaspoons:"))
+
 var convertToLiters = function(gallon) {
   return gallon*3.7854;
 };
 
-alert (convertToLiters(gallonInput) +" liters.");
+var convertToQuarts = function(cup) {
+  return cup * 4;
+};
+
+var convertToTeaspoons = function(tablespoon) {
+  return tablespoon*3;
+}
+
+alert (convertToLiters(gallonInput) + " liters.");
+alert (convertToQuarts(cupInput) + " quarts.");
+alert (convertToTeaspoons(tablespoonInput) + " teaspoons.")
